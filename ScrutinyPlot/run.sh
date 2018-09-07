2cf04628dc1c00ac94dbccfb7f7b906b9410f85b
Valentin <vkuznet@gmail.com>
2018-09-07 23:15:49 +0200

    New script to run whole workflow

---
+++ ScrutinyPlot/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+cmd=$PWD/mkscrutinydatafiles.sh
+
+# location of our data
+baseDir=/wma/vk/Monitoring/pop-data
+# location of phedex input dataframe, can be either in csv or csv.gz data-format
+phedexInput=$baseDir/2017/data/phedex_20170101_20180131.csv
+# location of dbs events input dataframe, can be either in csv or csv.gz data-format
+dbsInput=$baseDir/dbs_df.csv.gz
+#dbsInput=$baseDir/2017/data/dbs_events.csv
+# location of dbs_condor datasets, underlying files may be either in csv or csv.gz data-format
+dbsCondorInput=$baseDir/dbs_condor
+# title to apply to out plots
+title="T1/T2 Dataset Usage for 2017-2018"
+# generate image with format, supported png or pdf
+iformat=pdf
+
+# DO NOT EDIT BELOW THIS LINE
+
+#odir=/tmp/valya/popularity
+odir=`grep ^wkdir mkscrutinydatafiles.sh | awk '{split($1,a,"="); print a[2]}' | sed -s "s,\\$USER,$USER,g"`
+rm -rf $odir
+intervals=`egrep "^begindtrng|^enddtrng|^middt|^lastfracdt" $cmd | awk '{split($1,a,"="); print a[2]}' | sort | awk '{ORS=" "; print $0}'`
+date=`date +%Y%m%d`
+start_time="$(date -u +%s)"
+
+echo "Job run on date : $date (`date`)"
+echo "Phedex     input: $phedexInput"
+echo "DBS        input: $dbsInput"
+echo "DBS-Condor input: $dbsCondorInput"
+echo "Output area     : $odir"
+echo "intervals       : $intervals"
+echo "Title           : $title"
+$cmd $phedexInput $dbsInput $dbsCondorInput
+python plot3periods.py $odir/usesfullperiod.txt $odir/uses6month.txt $odir/uses3months.txt $iformat $title
+
+end_time="$(date -u +%s)"
+
+elapsed="$(($end_time-$start_time))"
+echo "Total of $elapsed seconds elapsed for process"
